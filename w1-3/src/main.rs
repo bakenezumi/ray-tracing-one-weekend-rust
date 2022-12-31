7a8193e4d1e27eb2d7a85b8fd6b4f611f4acebb3
hosonishi <hosonishi@gmail.com>
2022-12-31 18:29:22 +0900

    Simplifying the Ray-Sphere Intersection Code

--- w1-3/src/main.rs
+++ w1-3/src/main.rs
@@ -7,14 +7,15 @@ use ray::vec3::Point3;
 
 fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
   let oc = r.origin - center;
-  let a = r.direction.dot(&r.direction);
-  let b = oc.dot(&r.direction) * 2.0;
-  let c = oc.dot(&oc) - radius * radius;
-  let discriminant = b*b - 4.0*a*c;
+  let a = r.direction.length_squared();
+  let half_b = oc.dot(&r.direction);
+  let c = oc.length_squared() - radius * radius;
+  let discriminant = half_b*half_b - a*c;
+  
   if discriminant < 0.0 {
     -1.0
   } else {
-    return (-b - discriminant.sqrt()) / (2.0*a)
+    (-half_b - discriminant.sqrt()) / a
   }
 }
 
