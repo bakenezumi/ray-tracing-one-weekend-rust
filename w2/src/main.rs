600187f84a1b2def60c1f1d08f7cefcc04631b6b
hosonishi <hosonishi@gmail.com>
2023-02-17 12:47:03 +0900

    use tokio

--- w2/src/main.rs
+++ w2/src/main.rs
@@ -2,7 +2,8 @@ use std::sync::mpsc;
 use std::sync::Mutex;
 use rand::Rng;
 use rand::rngs::ThreadRng;
-use rayon::prelude::*;
+use rayon::iter::IntoParallelIterator;
+use rayon::prelude::ParallelIterator;
 use weekend::ray::Ray;
 use weekend::vec3::Vec3;
 use weekend::vec3::Color;
@@ -92,7 +93,8 @@ pub fn format_ppm(pixel_color: &Color, samples_per_pixel: i32) -> String {
   )
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
 
   let aspect_ratio = 16.0 / 9.0;
   let image_width = 384;
@@ -100,46 +102,55 @@ fn main() {
   let samples_per_pixel = 100;
   let max_depth = 50;
 
+  let (tx, rx) = mpsc::channel();
+  let mtx = Mutex::new(tx);
+
+  let image_generation_task = async move {
+    let world = {
+      let mut rng = Box::new(rand::thread_rng());
+      random_scene(&mut rng)
+    };
+  
+    let lookfrom = Vec3::new(13.0, 2.0, 3.0);
+    let lookat = Vec3::new(0.0, 0.0, 0.0);
+    let vup = Vec3::new(0.0, 1.0, 0.0);
+    let aspect_ratio = (image_width as f64)/(image_height as f64);
+    let dist_to_focus = 10.0;
+    let aperture = 0.1;
+  
+    let cam = Camera::new(lookfrom, lookat, vup, 20.0, aspect_ratio, aperture, dist_to_focus);
+  
+
+    (0 .. image_height).into_par_iter().for_each(|j| {
+      let mut rng = Box::new(rand::thread_rng());
+      for i in 0 .. image_width {
+        let mut pixel_color = Vec3 { x: 0.0, y: 0.0, z: 0.0 };
+        for _ in 0 .. samples_per_pixel {
+          let u = (i as f64 + rng.gen::<f64>()) / (image_width-1) as f64;
+          let v = (j as f64 + rng.gen::<f64>()) / (image_height-1) as f64;
+          let r = cam.get_ray(&mut rng, u, v);
+          pixel_color = pixel_color + ray_color(&mut rng, &r, &world, max_depth);
+        }
+        let ppm = format_ppm(&pixel_color, samples_per_pixel);
+        mtx.lock().unwrap().send(((image_height-j, i), ppm)).unwrap();
+      }
+    });
+  };  
+
+  tokio::spawn(image_generation_task);
+
   println!("P3");
   println!("{} {}", image_width, image_height);
   println!("255");
 
-  let world = {
-    let mut rng = Box::new(rand::thread_rng());
-    random_scene(&mut rng)
-  };
-
-  let lookfrom = Vec3::new(13.0, 2.0, 3.0);
-  let lookat = Vec3::new(0.0, 0.0, 0.0);
-  let vup = Vec3::new(0.0, 1.0, 0.0);
-  let aspect_ratio = (image_width as f64)/(image_height as f64);
-  let dist_to_focus = 10.0;
-  let aperture = 0.1;
-
-  let cam = Camera::new(lookfrom, lookat, vup, 20.0, aspect_ratio, aperture, dist_to_focus);
-
-  let (tx, rx) = mpsc::channel();
-  let mtx = Mutex::new(tx);
-  (0 .. image_height).into_par_iter().for_each(|j| {
-    let mut rng = Box::new(rand::thread_rng());
-    eprint!("\rScanlines remaining: {} ", j);
-    for i in 0 .. image_width {
-      let mut pixel_color = Vec3 { x: 0.0, y: 0.0, z: 0.0 };
-      for _ in 0 .. samples_per_pixel {
-        let u = (i as f64 + rng.gen::<f64>()) / (image_width-1) as f64;
-        let v = (j as f64 + rng.gen::<f64>()) / (image_height-1) as f64;
-        let r = cam.get_ray(&mut rng, u, v);
-        pixel_color = pixel_color + ray_color(&mut rng, &r, &world, max_depth);
-      }
-      let ppm = format_ppm(&pixel_color, samples_per_pixel);
-      mtx.lock().unwrap().send(((image_height-j, i), ppm)).unwrap();
-    }
-  });
-  
   let mut list = Vec::new();
+  let mut counter = 0;
+  let num_of_pixcels = image_height * image_width;
   for v in rx.iter() {
     list.push(v);
-    if image_height * image_width == list.len().try_into().unwrap() {
+    counter = counter + 1;
+    eprint!("\r{} %", counter * 100 / num_of_pixcels);
+    if num_of_pixcels == list.len().try_into().unwrap() {
       break;
     }
   }
